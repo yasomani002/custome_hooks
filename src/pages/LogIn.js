ce1d23a8bfa12c0455f0f18be3210be5d1edf693
Yash Somani <yasomani002@gmail.com>
2022-09-14 09:14:58 +0530

    remove clutter

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { makeStyles } from '@mui/styles';
 import { Grid, Typography } from '@mui/material';
 import useCustomeHooks from '../hooks/useCustomeHooks';
